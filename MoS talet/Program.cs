82353cd00e779051236acf9352fcf699db2a32a5
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:27:42 +0200

    Add files via upload

---
+++ MoS talet/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Skriv in ett tal :");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+int min = numbers[0];
+int max = numbers[0];
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }else if (numbers[i] > max) 
+    { 
+        max = numbers[i]; 
+    }
+}
+
+Console.WriteLine("Minsta talet är " + min);
+Console.WriteLine("Störst talet är " + max);
+
